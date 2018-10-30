11dcae5ada6a947bfd4a0012e07ae43d599816d6
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2018-10-30 13:06:02 +0100

    added basic bot, team, stage and tournament tables to createTables method

--- src/dk/aau/cs/ds306e18/tournament/database/CreateTables.java
+++ src/dk/aau/cs/ds306e18/tournament/database/CreateTables.java
@@ -8,9 +8,10 @@ import java.sql.Statement;
 public class CreateTables {
 
     public static void main(String[] args) {
+        createTables();
     }
 
-    private void testReadFromTable() {
+    static private void testReadFromTable() {
         Connection con = null;
         Statement stmt = null;
 
@@ -28,7 +29,7 @@ public class CreateTables {
         }
     }
 
-    private void testCreateTable() {
+    static private void createTables() {
 
         Connection con = null;
         Statement stmt = null;
@@ -37,17 +38,51 @@ public class CreateTables {
             con = DriverManager.getConnection("jdbc:h2:./database/db", "SA", "");
             stmt = con.createStatement();
 
-
-            stmt.executeUpdate("CREATE TABLE bot_tb (\n" +
-                    "   id INT NOT NULL,\n" +
-                    "   name VARCHAR(50) NOT NULL,\n" +
-                    "   developer VARCHAR(50) NOT NULL,\n" +
-                    "   configPath VARCHAR(50) NOT NULL,\n" +
-                    "   submissionDate DATE,\n" +
-                    "   PRIMARY KEY (id) \n" +
+            stmt.executeUpdate("CREATE TABLE Bot (\n" +
+                    "   Bot_id INT NOT NULL,\n" +
+                    "   Name VARCHAR(20) NOT NULL,\n" +
+                    "   Config_file VARCHAR(20) NOT NULL,\n" +
+                    "   Description VARCHAR(200) NOT NULL,\n" +
+                    "   PRIMARY KEY (Bot_id)\n" +
                     ");");
 
-            stmt.executeUpdate("insert INTO bot_tb VALUES (2, 'Beast from the east','Nicolaj', 'fuckIfIKnow',NOW())");
+
+
+            stmt.executeUpdate("CREATE TABLE Team (\n" +
+                    "   Team_id INT,\n" +
+                    "   Name VARCHAR(20) NOT NULL,\n" +
+                    "   Seed VARCHAR(30) NOT NULL,\n" +
+                    "   Description VARCHAR(200) NOT NULL,\n" +
+                    "   Bot_id INT NOT NULL,\n" +
+                    "   PRIMARY KEY (Team_id),\n" +
+                    "   FOREIGN KEY (Bot_id) REFERENCES Bot(Bot_id) \n" +
+                    ");");
+
+            stmt.executeUpdate("CREATE TABLE Stage (\n" +
+                    "   Stage_id INT NOT NULL,\n" +
+                    "   Name VARCHAR(20) NOT NULL,\n" +
+                    "   Format VARCHAR(20) NOT NULL,\n" +
+                    "   State VARCHAR(15) NOT NULL,\n" +
+                    "   SubmissionDate DATE,\n" +
+                    "   PRIMARY KEY (Stage_id) \n" +
+                    ");");
+
+            stmt.executeUpdate("CREATE TABLE Tournament (\n" +
+                    "   Tournament_id INT NOT NULL,\n" +
+                    "   Name VARCHAR(30) NOT NULL,\n" +
+                    "   Tiebreaker_rule VARCHAR(30) NOT NULL,\n" +
+                    "   State VARCHAR(15) NOT NULL,\n" +
+                    "   Stage_id INT NOT NULL,\n" +
+                    "   Team_id INT NOT NULL,\n" +
+                    "   PRIMARY KEY (Tournament_id), \n" +
+                    "   FOREIGN KEY (Stage_id) REFERENCES Stage(Stage_id), \n" +
+                    "   FOREIGN KEY (Team_id) REFERENCES Team(Team_id) \n" +
+                    ");");
+
+
+
+
+            //stmt.executeUpdate("insert INTO bot_tb VALUES (2, 'Beast from the east','Nicolaj', 'fuckIfIKnow',NOW())");
 
             con.commit();
 
